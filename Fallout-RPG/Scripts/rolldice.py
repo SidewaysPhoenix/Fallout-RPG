6eb3227fcf905352e8dfaeab15c3786f3c2a8d74
SidewaysPhoenix <madams2550@gmail.com>
2025-12-23 12:52:54 -0600

    vault backup: 2025-12-23 12:52:54

--- Fallout-RPG/Scripts/rolldice.py
+++ Fallout-RPG/Scripts/rolldice.py
@@ -16,8 +16,40 @@ damage_effect_y_n = input("Is there a damage effect for this roll? y/n\n")
 damage_effect_y_n = damage_effect_y_n.lower()
 
 
+def roll_selections(damage_effect_y_n):
+    damage_effect_options = ["Arc", "Breaking", "Burst", "Freeze", "Persistent", "Piercing", "Radioactive", "Spread", "Stun", "Vicious", "Cancel"]
+    
+    if damage_effect_y_n == "no" or damage_effect_y_n == "n":
+        print("No damage effects applied")
+        roll_dice(dice_to_roll)
+    
+    elif damage_effect_y_n == "yes" or damage_effect_y_n == "y":
+        #list out options for user to select from
+        for i in range(len(damage_effect_options)):
+            print(f"{i+1}). {damage_effect_options[i]}")
+        effect_option_select = input("Please select an option.\n")
+        effect_option_select = int(effect_option_select)
+        
+        #Check for invalid entry or cancelation
+        if (effect_option_select - 1) not in range(len(damage_effect_options)):
+            print("Invalid option selected")
+            sys.exit(0)
+        elif effect_option_select == 11:
+            print("Canceled")
+            sys.exit(0)
+        
+        results = roll_dice(dice_to_roll)
+
+        #Run option value variable as function
+        option_value = f'{damage_effect_options[effect_option_select-1].lower()}'
+        globals()[option_value](results)
+
+    else:
+        print("Invalid option selected.")
+        sys.exit(0)
+
+
 def roll_dice(dice_to_roll):
-    rolled_list = []
     #counts for each dice roll type
     one_count = 0
     two_count = 0
@@ -36,46 +68,21 @@ def roll_dice(dice_to_roll):
         elif current_roll == 5 or current_roll == 6:
             effect_count += 1
 
+    calculated_total = one_count + two_count + effect_count
     print(f"\nRolling {dice_to_roll} Combat Dice")
     print(f"\n    0's: {zero_count}\n    1's: {one_count}\n    2's: {two_count // 2} (2's Total: {two_count})\n    Effect's: {effect_count}")
-    print(f"\nTotal: {one_count + two_count + effect_count}\n")
-
+    print(f"\nTotal: {calculated_total}\n")
     
-    # sorted_dice = sorted(rolled_list)
+    return (one_count, two_count, zero_count, effect_count, calculated_total)
 
-def damage_effect_trigger(damage_effect_y_n):
-    damage_effect_options = ["Arc", "Breaking", "Burst", "Freeze", "Persistent", "Piercing", "Radioactive", "Spread", "Stun", "Vicious", "Cancel"]
-    
-    if damage_effect_y_n == "yes" or damage_effect_y_n == "y":
-        #list out options for user to select from
-        for i in range(len(damage_effect_options)):
-            print(f"{i+1}). {damage_effect_options[i]}")
-        effect_option_select = input("Please select an option.\n")
-        effect_option_select = int(effect_option_select)
-        
-        #Check for invalid entry or cancelation
-        if (effect_option_select - 1) not in range(len(damage_effect_options)):
-            print("Invalid option selected")
-            sys.exit(0)
-        if effect_option_select == 11:
-            print("Canceled")
-            sys.exit(0)
-        
-        #Run option value variable as function
-        option_value = f'{damage_effect_options[effect_option_select-1].lower()}'
-        globals()[option_value]()
 
-    elif damage_effect_y_n == "no" or damage_effect_y_n == "n":
-        print("No damage effects applied")
-        pass
-    
-    else:
-        print("Invalid option selected.")
-        sys.exit(0)
-
-def vicious():
+### Damage Effect Functions
+def vicious(results):
     print("Vicious applied")
+    print(f"{results[3]} Vicious Effect Damage")
+    print(f"{results[3] + results[4]} Total Damage with Vicious\n")
 
 
-damage_effect_trigger(damage_effect_y_n)
-roll_dice(dice_to_roll)
+
+
+roll_selections(damage_effect_y_n)
