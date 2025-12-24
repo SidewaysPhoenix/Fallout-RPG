155a159d027ee6876a94111c7a532c3502e78c69
SidewaysPhoenix <madams2550@gmail.com>
2025-12-23 20:26:11 -0600

    vault backup: 2025-12-23 20:26:11

--- Fallout-RPG/Scripts/rolldice.py
+++ Fallout-RPG/Scripts/rolldice.py
@@ -9,33 +9,39 @@ import random
 # 5 :: Effect
 # 6 :: Effect
 
-dice_to_roll = input("How many dice are we rolling?\n")
+dice_to_roll = input("\nHow many dice are we rolling?\n")
 dice_to_roll = int(dice_to_roll)
+print("-------------------------------------------------------\n")
 
-damage_effect_y_n = input("Is there a damage effect for this roll? y/n\n")
+damage_effect_y_n = input("\nIs there a damage effect for this roll? y/n\n")
 damage_effect_y_n = damage_effect_y_n.lower()
-
+print("-------------------------------------------------------\n")
 
 def roll_selections(damage_effect_y_n):
     damage_effect_options = ["Arc", "Breaking", "Burst", "Freeze", "Persistent", "Piercing", "Radioactive", "Spread", "Stun", "Vicious", "Cancel"]
     
     if damage_effect_y_n == "no" or damage_effect_y_n == "n":
-        print("No damage effects applied")
+        print("-------------------------------------------------------\n")
+        print("\nNo damage effects applied\n")
+        print(f"*** You hit the {roll_location()} ***\n")
         roll_dice(dice_to_roll)
     
     elif damage_effect_y_n == "yes" or damage_effect_y_n == "y":
         #list out options for user to select from
         for i in range(len(damage_effect_options)):
             print(f"{i+1}). {damage_effect_options[i]}")
-        effect_option_select = input("Enter option(s) seperated by commas ex: 3,5,10\n")
+        effect_option_select = input("\nEnter option(s) seperated by commas ex: 3,5,10\n")
         effect_option_select = effect_option_select.replace(" ", "")
         effect_option_select = effect_option_select.split(",")
+        print("-------------------------------------------------------\n")
         
         results = roll_dice(dice_to_roll)
         if results[3] == 0:
             print("No effects rolled, damage effects will not apply.")
             return
-
+        print("/ --------------- /")
+        print("/ Applied Effects /")
+        print("/ --------------- /\n")
         for i in effect_option_select:
             selected_option = int(i)
 
@@ -50,6 +56,8 @@ def roll_selections(damage_effect_y_n):
             #Run option value variable as function
             option_value = f'{damage_effect_options[selected_option-1].lower()}'
             globals()[option_value](results)
+
+        print("-------------------------------------------------------\n")
 
     else:
         print("Invalid option selected.")
@@ -78,11 +86,17 @@ def roll_dice(dice_to_roll):
     print(f"\n*** Rolling {dice_to_roll} Combat Dice ***")
     print(f"\n    0's: {zero_count}\n    1's: {one_count}\n    2's: {two_count // 2} (2's Total: {two_count})\n    Effect's: {effect_count}")
     print(f"\n*** Total: {calculated_total}***\n ")
+    print("-------------------------------------------------------\n")
     
     return (one_count, two_count, zero_count, effect_count, calculated_total)
 
-def roll_location(location_dice_to_roll):
-    # 1–2 Head 3–8 Torso 9–11 Left arm 12–14 Right arm 15–17 Left Leg 18–20 Right Leg
+def roll_location():
+    # 1–2 Head 
+    # 3–8 Torso 
+    # 9–11 Left arm 
+    # 12–14 Right arm 
+    # 15–17 Left Leg 
+    # 18–20 Right Leg
     location_roll = random.randrange(1,21)
     if location_roll in range(1,3):
         return "Head"
@@ -99,43 +113,47 @@ def roll_location(location_dice_to_roll):
 
 ### Damage Effect Functions
 def arc(results):
-    print("*** Arc Applied ***")
+    print("*** Arc ***")
     print(f"You automatically hit {results[3]} additional targets in Close range of the primary target.\n")
 
 def breaking(results):
-    print("*** Breaking Applied ***")
+    print("*** Breaking ***")
     print(f"If target is in cover reduce the Cover Combat Die amount by {results[3]} permanently.\nIf the target is not in cover, instead reduce the DR of the location struck by 1, according to the damage type of the weapon. Ex: physical damage only reduces physical DR.\n")
 
 def burst(results):
-    print("*** Burst Applied ***")
+    print("*** Burst ***")
     print(f"The attack hits {results[3]} additional targets, for each additional target spend 1 additional unit of ammunition from the weapon.\n")
 
 def freeze(results):
-    print("*** Freeze Applied ***")
+    print("*** Freeze ***")
     print(f"Enemies will be frozen if half of their END or Body score is less than or equal to {results[3]}.\nA Frozen creature cannot take actions on its next turn.\n")
 
 def persistent(results):
-    print("*** Persistent Applied ***")
+    print("*** Persistent ***")
     print(f"The target suffers the weapon’s damage again at the end of their next and subsequent turns, for {results[3]} rounds.\nThe target can spend a major action to make a test to stop persistent damage early, with a difficulty of {results[3]}, and the attribute + skill chosen by the GM.\nSome Persistent weapons may inflict a different type of damage to the weapon, and where this is the case, it will be noted in brackets, for example: Persistent (Poison).\n")
 
 def piercing(results):
-    print("*** Piercing Applied ***")
+    print("*** Piercing ***")
     print(f"Ignore {results[3]} points of the target’s DR.\n")
 
 def radioactive(results):
-    print("*** Radioactive Applied ***")
+    print("*** Radioactive ***")
     print(f"The target also suffers {results[3]} point(s) of radiation damage.\nThis radiation damage is totalled and applied separately, after a character has suffered the normal damage from the attack.\n")
 
 def spread(results):
-    print("*** Spread Applied ***")
-    print(f"Your attack inflicts {results[3]} additional hit(s) on the target.\nEach additional hit inflicts {results[3] // 2} damage and hits a random location even if a specific location was targeted for the initial attack.\n")
+    print("*** Spread ***")
+    location_list = []
+    for i in range(results[3]):
+        location_list.append(roll_location())
+    print(f"Your attack inflicts {results[3]} additional hit(s) on the target.\nEach additional hit inflicts {results[3] // 2} damage to the following random locations even if a specific location was targeted for the initial attack.")
+    print(f"{location_list}\n")
 
 def stun(results):
-    print("*** Stun Applied ***")
+    print("*** Stun ***")
     print("The target cannot take their normal actions on their next turn.\nA stunned character or creature can still spend AP to take additional actions as normal.")
 
 def vicious(results):
-    print("*** Vicious Applied ***")
+    print("*** Vicious ***")
     print(f"{results[3]} Vicious Effect Damage")
     print(f"{results[3] + results[4]} Total Damage with Vicious\n")
 
