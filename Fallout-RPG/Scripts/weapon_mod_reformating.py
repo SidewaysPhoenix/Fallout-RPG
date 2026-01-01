94e14de4c0d0ae3327798015a067e1bc8ce93cff
SidewaysPhoenix <madams2550@gmail.com>
2025-12-31 18:33:04 -0600

    vault backup: 2025-12-31 18:33:04

--- Fallout-RPG/Scripts/weapon_mod_reformating.py
+++ Fallout-RPG/Scripts/weapon_mod_reformating.py
@@ -5,14 +5,16 @@ import re
 dir_path = os.path.dirname(os.path.realpath(__file__))
 main_mod_path = os.path.join(dir_path, "Weapon_Mods") 
 
+folders_traversed = []
 files = []
 def path_crawl(path):
     for i in list_files(path):
         current_path = os.path.join(path, i)
         if os.path.isdir(current_path):
+            folders_traversed.append(os.path.basename(current_path))
             path_crawl(current_path)
         elif os.path.isfile(current_path):
-            #files.append(os.path.basename(current_path))
+            files.append(os.path.basename(current_path))
             file_parser(current_path)
 
 
@@ -70,14 +72,17 @@ def string_classifier(string):
         return mod_fire_rate(string)
     elif is_mod_range(string):
         return mod_range(string)
+    elif is_mod_ammo(string):
+        return mod_ammo(string)
+
     else:
         return None
 
 #Damage parsing
 def is_mod_dmg(string):
-    if re.search("\+[0-9]+ d6",string) or re.search("\-[0-9]+ d6", string):
+    if re.search(r"\+[0-9]+ d6",string) or re.search(r"\-[0-9]+ d6", string):
         return True
-    elif re.search("\+[0-9]+d6",string) or re.search("\-[0-9]+d6", string):
+    elif re.search(r"\+[0-9]+d6",string) or re.search(r"\-[0-9]+d6", string):
         return True
 
 def mod_dmg(damage_string):
@@ -85,27 +90,27 @@ def mod_dmg(damage_string):
     raw_damage_string = no_spaces_string.replace('"', '')
     cleaned_line = raw_damage_string.replace("damage", "")
 
-    if re.search("\+[0-9]+d6", cleaned_line) or re.search("\-[0-9]+d6", cleaned_line):
+    if re.search(r"\+[0-9]+d6", cleaned_line) or re.search(r"\-[0-9]+d6", cleaned_line):
         return f'mod_dmg: "{cleaned_line}"'
     else:
-        return damage_string
+        return None
 
 #Fire Rate parsing
 def is_mod_fire_rate(string):
-    if re.search("[0-9]+\s+[A-Za-z]+\s[A-Za-z]+",string):
+    if re.search(r"[0-9]+\s+[A-Za-z]+\s[A-Za-z]+",string):
         return True
 
 def mod_fire_rate(fire_rate_string):
     clear_fire_rate_text = fire_rate_string.replace("fire rate", "")
     raw_fire_rate_string = clear_fire_rate_text.replace(" ", "")
-    if re.search("\+[0-9]+",raw_fire_rate_string) or re.search("\-[0-9]+",raw_fire_rate_string):
+    if re.search(r"\+[0-9]+",raw_fire_rate_string) or re.search(r"\-[0-9]+",raw_fire_rate_string):
         return f'mod_fire_rate: "{raw_fire_rate_string}"'
     else:
-        return(fire_rate_string)
+        return None
     
 #Range parsing
 def is_mod_range(string):
-    if re.search("(\s+([A-Za-z]+\s+)+)",string):
+    if re.search(r"range",string):
         return True
     
 def mod_range(range_string):
@@ -117,18 +122,46 @@ def mod_range(range_string):
     elif "increase" in step_removed:
         raw_range = step_removed.replace("increase", "+")
     
-    if re.search("\+[0-9]+", raw_range) or re.search("\-[0-9]+",raw_range):
+    if re.search(r"\+[0-9]+", raw_range) or re.search(r"\-[0-9]+", raw_range):
         return f'mod_range: "{raw_range}"'
     else:
-        return range_string
+        return None
 
+#Ammo parsing
+def is_mod_ammo(string):
+    if re.search(r"ammo to", string) or re.search(r"ammo changes to", string):
+        return True
+
+def mod_ammo(ammo_string):
+    if re.search(r"ammo", ammo_string):    
+        clean_string_list = ammo_string.split(" ")
+        final_list = []
+        for i in clean_string_list:
+            string_piece = i.strip()
+            if re.search(r"ammo", string_piece):
+                pass
+            elif re.search(r"changes", string_piece):
+                pass
+            elif re.search(r"to", string_piece):
+                pass
+            else:
+                final_list.append(string_piece)
+
+        final_string = " ".join(final_list)
+        return f'mod_ammo: "{final_string}"'
+    else:
+        return None
 
 path_crawl(main_mod_path)
 
 
+#Damage Effects List [arc, breaking, burst, freeze, persistent, piercing, radioactive, spread, stun, vicious]
+#Weapon Qualities List [accurate, ammo-hungry, blast, bombard, close quarters, concealed, debilitating, delay, gatling, inaccurate, mine, night vision, parry, placed, recoil, recon, reliable, slow load, suppressed, surge, thrown, two-handed, unreliable]
+#Weapon Type List [big guns, energy weapons, explosives, melee weapons, small guns, throwing, unarmed]
+#Weapon Damage Type List [physical, energy, radiation, poison]
+
 #mod_qualities:
-#mod_dmg_effect: +[[Piercing]] 1, +
-#mod_range:
+#mod_dmg_effect: +[[Piercing]] +1
 #mod_ammo:
 #mod_dmg_type:
 #mod_weapon_type: bayonet will be effect only.
