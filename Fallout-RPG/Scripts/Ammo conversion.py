4109b6a5c52704fae500d4c23de9ee7d85f1d8d2
SidewaysPhoenix <madams2550@gmail.com>
2026-01-11 16:33:40 -0600

    vault backup: 2026-01-11 16:33:40

--- Fallout-RPG/Scripts/Ammo conversion.py
+++ Fallout-RPG/Scripts/Ammo conversion.py
@@ -3,70 +3,75 @@ import sys
 import re
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-main_mod_path = os.path.join(dir_path, "Weapon_Mods") 
+main_mod_path = os.path.join(dir_path, "Weapons") 
 
 
-
+Weapon_Ammo_Associations = {}
 folders_traversed = []
 files = []
 def path_crawl(path):
+
     for i in list_files(path):
         current_path = os.path.join(path, i)
         if os.path.isdir(current_path):
             folders_traversed.append(os.path.basename(current_path))
             path_crawl(current_path)
+        
         elif os.path.isfile(current_path):
             files.append(os.path.basename(current_path))
-            file_parser(current_path)
+            NameAmmo_from_file = file_parser(current_path)
+            
+            if NameAmmo_from_file[0] in Weapon_Ammo_Associations:
+                previous_entry = Weapon_Ammo_Associations[NameAmmo_from_file[0]]
+                Weapon_Ammo_Associations[NameAmmo_from_file[0]] = (f"{previous_entry} / {NameAmmo_from_file[1]}")
+            else:
+                Weapon_Ammo_Associations[NameAmmo_from_file[0]] = NameAmmo_from_file[1]
+
 
 def list_files(path):
     return os.listdir(path)
 
 
 
-def file_parser(file_to_read):
+def file_parser(file_to_read): #return [Ammo, Name]
     file = open(file_to_read, "r", encoding="utf-8")
     content = file.read()
     content = content.strip()
     line_list = content.split("\n")
-    '''
-    new_yaml_lines = {
-        "Damage": "",
-        "Fire Rate": "",
-        "Range": "",
-        "Change Ammo Type": "",
-        "Change Base Damage To": "",
-        "Weapon Damage Effects": "",
-        "Change Damage Type To": "",
-        "Weapon Qualities": "",
-        "Effects": "",
-    }
-    '''
-
+    captured_name = ""
+    captured_ammo = ""
     content_rebuild_list = []
     content_rebuild_list.append("```statblock") #apply starter codeblock backticks
     for i in range(1, len(line_list)-1):
-        if line_list[i].startswith("ammo:"):
-            ammo_to_pass = line_list[i].replace("ammo: ", "")
-            
-
-            #parsed_ammo_list = ammo_parser(ammo_to_pass) #start effect parser here
-            
-            if ammo_to_pass == "": #empty effects cleared out here
+        if line_list[i].startswith("name:"):
+            content_rebuild_list.append(line_list[i])
+            name_to_pass = line_list[i].replace("name: ", "").strip('"')
+            if name_to_pass == "":
                 continue
-
-            for i in parsed_ammo_list:
-                content_rebuild_list.append(f'ammo: "{i}"')
+            else:
+                captured_name = name_to_pass
+                
+        elif line_list[i].startswith("ammo:"):
+            content_rebuild_list.append(line_list[i])
+            ammo_to_pass = line_list[i].replace("ammo: ", "").strip('"')
+            if ammo_to_pass == "":
+                captured_ammo = "N/A"
+            else:
+                captured_ammo = ammo_to_pass
         
         else:
             content_rebuild_list.append(line_list[i])
     
+    if captured_ammo == "":
+        captured_ammo = "No Ammo"
+
     content_rebuild_list.append("```") #reapply codeblock backticks
     content_rebuild = '\n'.join(content_rebuild_list)    
     print(content_rebuild)
     #with open(file_to_read, "w", encoding="utf-8") as f:
         #f.write(content_rebuild)    
     file.close()
+    return [captured_ammo, captured_name]
 
 '''
 def ammo_parser(effect_line):
@@ -117,6 +122,10 @@ def string_classifier(string):
 
 path_crawl(main_mod_path)
 
+for i in sorted(Weapon_Ammo_Associations):
+    print(f"{i}: {Weapon_Ammo_Associations[i]}\n")
+
+#print(Weapon_Ammo_Associations)
 
 
 
